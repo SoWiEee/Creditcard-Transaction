d24e21fba91f7ec7eec5a4b0b656b992e0b34396
SoWiEee <kawiisowieee@gmail.com>
2025-12-16 20:46:30 +0800

    Add middleware for protection

--- backend/src/routes/transactionRoutes.js
+++ backend/src/routes/transactionRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import * as txController from '../controllers/transactionController.js';
+import { validate } from '../middleware/validate.js';
+import { paySchema, transactionActionSchema } from '../utils/schemas.js';
 
 const router = express.Router();
 
-router.post('/pay', txController.pay);
-router.post('/void', txController.voidTx);
-router.post('/refund', txController.refundTx);
+router.post('/pay', validate(paySchema), txController.pay);
+router.post('/void', validate(transactionActionSchema), txController.voidTx);
+router.post('/refund', validate(transactionActionSchema), txController.refundTx);
 
 export default router;

10bd3f8a7b93628a377cdaa26ada89d840b100eb
SoWiEee <kawiisowieee@gmail.com>
2025-12-17 16:41:01 +0800

    Add Points table

--- db/init.sql
+++ db/init.sql
@@ -11,9 +11,21 @@ CREATE TABLE IF NOT EXISTS Transactions (
     user_id INT NOT NULL,
     amount DECIMAL(10, 2) NOT NULL,
     status VARCHAR(20) CHECK (status IN ('Pending','Paid','Voided','Refunded')) NOT NULL,
+    merchant VARCHAR(50),
     point_change INT DEFAULT 0,
     source_transaction_id INT DEFAULT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (source_transaction_id) REFERENCES Transactions(transaction_id)
+);
+
+CREATE TABLE IF NOT EXISTS Points (
+    log_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    transaction_id INT,
+    change_amount INT NOT NULL,
+    reason VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (transaction_id) REFERENCES Transactions(transaction_id)
 );
